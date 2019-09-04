73ec5a1b52b1b8272e0b67011226a2be56c4d50f
jasongama <jasongama967@gmail.com>
2019-09-04 17:42:46 +0200

    almost done

--- shoes.js
+++ shoes.js
@@ -7,34 +7,59 @@ const sizeElement = document.querySelector(".sizeStyle");
 
 var kicksFactory = ShoeFactory();
  
-var KicksTemplate = document.querySelector(".KicksTemplate").innerHTML;
+// var KicksTemplate = document.querySelector(".KicksTemplate").innerHTML;
 
-var userTemplate = Handlebars.compile(KicksTemplate);
+// var userTemplate = Handlebars.compile(KicksTemplate);
 
-var userDataElem = document.querySelector(".Kicks");
+// var userDataElem = document.querySelector(".Kicks");
 
+if(kicksFactory.brand){
+    let newShoe = {shoe}
+}
+
+function brands(list){
+    let shoeTemplate = document.querySelector(".brandTemplate").innerHTML 
+    let shoeBrandTemplate= Handlebars.compile(shoeTemplate)
+    let printShoes= shoeBrandTemplate(list) 
+    document.querySelector(".brand").innerHTML=printShoes;
+}
 function  brandFunction(){
 
+console.log(brandElement.value);
+console.log(colorElement.value);
+console.log(sizeElement.value);
 
 
 }
+
+
+
+
+
+
+
 
 function shoesFilters(){
     const currentBrand = brandElement.value;
     const  currentColor = colorElement.value;
+    const currentSize = sizeElement.value;
+
     let filteredShoes=[];
-     
+    if(currentBrand ==="" && currentColor === "" && currentSize ===""){
+     filteredShoes =kicksFactory.addedshoes()
+    }
+     else{
     const shoesStore ={
         brand: currentBrand,
-        color:currentColor
+        color: currentColor,
+        size: currentSize
     };
-    const filteredShoes = kicksFactory.shoeFliter(shoesStore);
-    for(var i=0; i < filteredShoes.length; i++){
-        const currentKicks =filteredShoes[i];
+    filteredShoes =kicksFactory.shoeFliter(shoesStore);
 
-        const newKicks 
-    } 
+  }
 
 
 
 }
+
+addBtn.addEventListener("click",brandFunction );
